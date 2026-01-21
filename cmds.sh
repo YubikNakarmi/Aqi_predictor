9b3fd08f998a82f332c14d8035340b7d88b41bea
yubik <yubikprakash@gmail.com>
2026-01-21 21:22:30 +0545

    cleaning wrappers and using mlflow in wrappers instead of library

--- cmds.sh
+++
@@ -1,7 +0,0 @@
-docker compose build --pull --no-cache 
-
-mlflow server --backend-store-uri sqlite:///db/mlflow.db \
---default-artifact-root wasbs://mlflow@yubikaqidata.blob.core.windows.net/ --port 5000
-
-mlflow server --backend-store-uri pymysql://root:yubik123@localhost:3306/mlflow_backend \
---default-artifact-root wasbs://mlflow@yubikaqidata.blob.core.windows.net/ --port 5000
