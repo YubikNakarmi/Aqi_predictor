a364f96d0d52e190ddc234dc2bf34f7e994b36ac
yubik <yubikprakash@gmail.com>
2026-03-22 20:12:11 +0545

    readme

--- frontend/app.py
+++ frontend/app.py
@@ -2,6 +2,14 @@ import streamlit as st
 import pandas as pd
 
 
+
+
+
 st.set_page_config(page_title="Air Quality Dashboard", layout="wide", page_icon=":cloud:")
+
+
+
+
 st.write("# Air Quality Dashboard :cloud:")
 st.sidebar.success("Welcome to the Air Quality Dashboard! :wave:")
+st.title("Daily Air Quality Index (AQI) Forecasts")
