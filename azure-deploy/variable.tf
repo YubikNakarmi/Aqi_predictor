71bbe4b195a8881e37a6c2775d2c280459357d7e
yubik <yubikprakash@gmail.com>
2026-04-04 16:29:21 +0545

    readme

--- azure-deploy/variable.tf
+++ azure-deploy/variable.tf
@@ -23,13 +23,13 @@ variable "admin-name" {
 }
 
 variable "my-ip" {
-    description = "Your public IP address for NSG rule (CIDR format)"
+    description = "Public IP address for NSG rule (CIDR format)"
     type        = string
     default     = "0.0.0.0/0"
 }
 
-variable "ssh-key-path" {
-    description = "Path to your SSH public key"
+variable "public-key" {
+    description = "SSH public key"
     type        = string
     default     = "" #-var="ssh_public_key=$SSH_PUBLIC_KEY"
 }
