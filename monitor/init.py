178b1d3f8a0b3eac8cd8ddb59fd48a6b052dc264
yubik <yubikprakash@gmail.com>
2026-04-18 18:19:21 +0545

    monitoring init

--- monitor/init.py
+++ monitor/init.py
@@ -1,1 +1,15 @@
 import nannyml
+import os
+import pandas as pd
+
+DATA_PROCESSED_DIR = os.getenv("DATA_PROCESSED_PATH", r"data/processed/hourly/us_paro_hourly")
+
+
+def estimator(reference_df, analysis_df):
+    
+    reference_df = pd.read_parquet(os.path.join(DATA_PROCESSED_DIR, "val_processed.parquet"))
+
+    estimate = nannyml.DLE()
+
+
+def main():
