6752ddaf2e8afc6274f3c4469ed2821071b64a76
yubik <yubikprakash@gmail.com>
2026-05-08 13:39:03 +0545

    dvc

--- monitor/init.py
+++ monitor/init.py
@@ -5,6 +5,10 @@ from src.modules.monitoring import Monitoring, plots
 DATA_PROCESSED_DIR = os.getenv("DATA_PROCESSED_PATH", r"data/processed/hourly/us_paro_hourly")
 
 
+def dataload_live():
+    pass
+
+
 def main():
     monitor = Monitoring(reference_df=pd.read_csv(os.path.join(DATA_PROCESSED_DIR, "reference.csv")),
                         analysis_df=pd.read_csv(os.path.join(DATA_PROCESSED_DIR, "analysis.csv")),)
